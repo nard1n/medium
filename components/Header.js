dd41aa18ba8359bd805f8ab132df91af9619b88b
Nardin Lachowski <nardin@live.com>
2022-07-09 11:57:25 -0400

    added logo and banner buttons

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,21 @@ const Header = () => {
     return(
         <div className={styles.wrapper}>
             <div className={styles.content}>
-                <h1>Getting started...</h1>
+                <div className={styles.logoContainer}>
+                    <Image 
+                        className={styles.logo}
+                        src={Logo}
+                        height={40}
+                        width={200}
+                        alt="logo"
+                    />
+                </div>
+                <div className={styles.bannerNav}>
+                    <div>Our Story</div>
+                    <div>Membership</div>
+                    <div>Sign In</div>
+                    <div>Get Started</div>
+                </div>
             </div>
         </div>
     )
