fb6c73e811ffe40a80ecdeaae9895d67593d14a9
Nardin Lachowski <nardin@live.com>
2022-07-09 12:25:10 -0400

    styled header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
-const styles = {}
-
 import Image from 'next/image';
-import Logo from '../static/logo.png'
+import Logo from '../static/logo.png';
+
+const styles = {
+    wrapper: 'flex justify-center gap-10 p-5 bg-[#FCC017]',
+    content: 'max-w-7xl flex-1 flex justify-between gap-10',
+    logoContainer: 'flex items-center flex-start',
+    logo: 'cursor-poiner object-contain',
+    bannerNav: 'flex cursor-pointer items-center space-x-5',
+    accentedButton: 'bg-black text-white py-2 px-4 rounded-full',
+}
 
 const Header = () => {
     return(
@@ -20,7 +27,7 @@ const Header = () => {
                     <div>Our Story</div>
                     <div>Membership</div>
                     <div>Sign In</div>
-                    <div>Get Started</div>
+                    <div className={styles.accentedButton}>Get Started</div>
                 </div>
             </div>
         </div>
