a6bb3d0a533287b207f58ba54faeba71c4958a41
Nardin Lachowski <nardin@live.com>
2022-07-10 12:32:54 -0400

    completed the styling of the banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,33 @@
+import Image from 'next/image';
+import Logo from './../static/banner.png';
+
+const styles = {
+    accentedButton: 'bg-black text-white py-2 px-4 rounded-full',
+    content: 'max-w-7xl flex-1 flex items-center justify-between',
+    wrapper: 'h-max-[10rem] flex items-center justify-center bg-[#FCC017] border-y border-black',
+}
+
 const Banner = () => {
     return(
-        <h1 className='max-w-xl text-[6rem] font-mediumSerif'>Stay Curious.</h1>
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className="space-y-5 px-10 flex-[3]">
+                    <h1 className='max-w-xl text-[6rem] font-mediumSerif'>Stay curious.</h1>
+
+                    <h3 className='text-2xl'>Discover stories, thinking, and expertise from writers on any topic.</h3>
+
+                    <button className={styles.accentedButton}>Start Reading</button>
+                </div>
+
+                <Image
+                    className='hidder h-32 md:inline-flex object-contained flex-1'
+                    src={Logo.src}
+                    alt="banner image"
+                    width={500}
+                    height={400}
+                />
+            </div>
+        </div>
     )
 }
 
