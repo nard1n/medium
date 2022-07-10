a1b28c944c8fee256ccf8098df25789443204fe1
Nardin Lachowski <nardin@live.com>
2022-07-10 12:47:39 -0400

    passing the postcard component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import PostCard from "../components/postCard";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <PostCard />
     </>
   )
 }
