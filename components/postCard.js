238a17bf52f54dfe8969883549c6969bea99b1ba
Nardin Lachowski <nardin@live.com>
2022-07-12 11:25:38 -0400

    added thumbnail to post cards

--- components/postCard.js
+++ components/postCard.js
@@ -4,6 +4,7 @@ import { FiBookmark } from 'react-icons/fi';
 
 
 const styles = {
+    wrapper: `flex max-w-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer`,
     authorContainer: `flex gap-[.4rem]`,
     authorName: `font-semibold`,
     authorImageContainer: `grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]`,
@@ -14,31 +15,43 @@ const styles = {
     articleDetails:`my-2 text-[.8rem]`,
     postDetails: ``,
     category:`bg-[#F2F3F2] rounded-full p-1`,
-    bookmarkContainer:``,
+    bookmarkContainer:`cursor-pointer`,
+    thumbnailContainer:``,
 }
 const PostCard = () => {
     return (
-        <div className={styles.postDetails}>
-            <div className={styles.authorContainer}>
-                <div className={styles.authorImageContainer}>
-                    <Image
-                        src={Logo}
-                        alt="author image"
-                        className={styles.authorImage} 
-                        height={40}
-                        width={40}          
-                    />
+        <div className={styles.wrapper}>
+            <div className={styles.postDetails}>
+                <div className={styles.authorContainer}>
+                    <div className={styles.authorImageContainer}>
+                        <Image
+                            src={Logo}
+                            alt="author image"
+                            className={styles.authorImage} 
+                            height={40}
+                            width={40}          
+                        />
+                    </div>
+                    <div className={styles.authorName}>Nardin L</div>
                 </div>
-                <div className={styles.authorName}>Nardin L</div>
+                <h1 className={styles.title}>7 Free Tools to Increase Productivity in 2022</h1>
+                <div className={styles.briefing}>Productivity is a skill that can be learned</div>
+                <div className={styles.detailsContainer}>
+                    <span className={styles.articleDetails}>Jul 11 • 5 min read • <span className={styles.category}>productivity</span></span>
+                    <span  className={styles.bookmarkContainer}>
+                        <FiBookmark className='h-5 w-5'/>
+                    </span>
+                </div>
             </div>
-            <h1 className={styles.title}>7 Free Tools to Increase Productivity in 2022</h1>
-            <div className={styles.briefing}>Productivity is a skill that can be learned</div>
-            <div className={styles.detailsContainer}>
-                <span className={styles.articleDetails}>Jul 11 • 5 min read • <span className={styles.category}>productivity</span></span>
-                <span  className={styles.bookmarkContainer}>
-                    <FiBookmark className='h-5 w-5'/>
-                </span>
+            <div className={styles.thumbnailContainer}>
+                <Image 
+                height={100}
+                width={100}
+                alt='thumbnail'
+                src={Logo}
+                />
             </div>
+
         </div>
     )
 }
