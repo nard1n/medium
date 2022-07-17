a1674c89771e042ba72bfa0d232a8e941f76121f
Nardin Lachowski <nardin@live.com>
2022-07-16 20:52:30 -0400

    added flex for content styling

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,16 @@
 import ReadersNav from '../../components/ReadersNav';
 import Recommendations from '../../components/Recommendations';
+
+const styles = {
+    content: `flex`,
+}
+
 const Post = () => {
     return (
-        <>
+        <div className={styles.content}>
             <ReadersNav />
             <Recommendations />
-        </>
+        </div>
     )
 }
 
