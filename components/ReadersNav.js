e8253ccca02cf817ac08dbd2d909e37f266479cb
Nardin Lachowski <nardin@live.com>
2022-07-16 20:43:56 -0400

    completed styling, corrected divider div closing tag

--- components/ReadersNav.js
+++ components/ReadersNav.js
@@ -12,9 +12,9 @@ const styles = {
     logoContainer: `cursor-pointer`,
     wrapper: `w-[5rem] h-screen flex flex-col justify-between items-center p-[1rem]`,
     iconsContainer: `flex-1 flex flex-col justify-center gap-[1.4rem] text-2xl text-[#787878]`,
-    divider: ``,
-    profileImageContainer: ``,
-    profileImage: ``,
+    divider: `border-b`,
+    profileImageContainer: `w-[2.4rem] h-[2.4rem] rounded-full overflow-hidden place-items-center`,
+    profileImage: `object-cover`,
 }
 
 const ReadersNav = () => {
@@ -30,15 +30,14 @@ const ReadersNav = () => {
                 <FiBell />
                 <BiBookmarks />
                 <RiArticleLine />
-                <div className={styles.divider}>
-                    <BsPencilSquare />
-                </div>
-                <div className={styles.profileImageContainer}>
-                    <Image className={styles.profileImage}
-                    src={UserImage}
-                    alt="user's profile image"
-                    />
-                </div>
+                <div className={styles.divider} />
+                <BsPencilSquare />
+            </div>
+            <div className={styles.profileImageContainer}>
+                <Image className={styles.profileImage}
+                src={UserImage}
+                alt="user's profile image"
+                />
             </div>
         </div>
     )
