e0d3343d6fb3a1d98ab73ccb784242f3adc233eb
Nardin Lachowski <nardin@live.com>
2022-07-22 20:57:54 -0400

    added article content

--- components/ArticleMain.js
+++ components/ArticleMain.js
@@ -6,11 +6,12 @@ import { GrLinkedin } from 'react-icons/gr';
 import { HiOutlineLink } from 'react-icons/hi';
 import { BiBookmarks } from 'react-icons/bi';
 import { FiMoreHorizontal } from 'react-icons/fi';
+import Banner from '../static/banner.png';
 import UserImage from '../static/user.png';
 
 const styles ={
     wrapper: `flex items-center justify-center flex-[3] border-l border-r`,
-    content: `h-screen p-[2rem]`,
+    content: `h-screen w-full p-[2rem]`,
     postHeaderContainer: `flex justify-between items-center mt-[2.2rem] mb-[1.2rem]`,
     authorContainer: ` flex gap-[1rem]`,
     authorProfileImageContainer: `h-[3rem] w-[3rem] rounded-full overflow-hidden grid center`,
@@ -19,8 +20,12 @@ const styles ={
     listenButton: `flex items-center gap-[.2rem] text-[#1A8917]`,
     socials: `flex gap-[1rem] text-[#787878] cursor-pointer`,    
     space: `w-[.5rem]`,
-    articleMainContainer: ``,
-    bannerContainer: ``,    
+    articleMainContainer: `flex flex-col gap-[1rem]`,
+    bannerContainer: `h-[18rem] w-full grid center overflow-hidden mb-[2rem]`,    
+    image: `object-cover`,
+    title: `font-bold text-3xl `,
+    subtitle: `font-mediumSerifItalic text-[1.3rem] text-[#292929]`,
+    articleText: `font-mediumSerif text-[1.3rem] text-[#292929]`,
 }
 
 const ArticleMain = () => {
@@ -39,7 +44,6 @@ const ArticleMain = () => {
                                 <AiFillPlayCircle /> Listen
                             </span>
                         </div>
-
                     </div>
                 </div>
                 <div className={styles.socials}>
@@ -54,7 +58,25 @@ const ArticleMain = () => {
             </div>
             <div className={styles.articleMainContainer}>
                 <div className={styles.bannerContainer}>
-                     {/* <Image /> */}
+                     <Image 
+                     className={styles.image}
+                     src={Banner}
+                     height={100}
+                     width={100}
+                     alt="Banner"
+                     />
+                </div>
+                <h1 className={styles.title}>7 Free Tools to Increase Productivity in 2022</h1>
+                <h4 className={styles.subtitle}>
+                    <div>
+                        Nardin Codes, July 20, 2022
+                    </div>
+                    <div>Brief: Productivity is a skill that can be learned.</div>    
+                </h4>
+                <div className={styles.articleText}>
+                    Lorem ipsum dolor sit amet. Est iure doloremque rem dolorem maxime est deleniti dolores et nihil quisquam sed obcaecati quae ut nihil dolor et omnis minima. Ea atque aspernatur et praesentium obcaecati ut itaque nisi et consequatur voluptate eum aliquid iste. Sed voluptates minus sed doloribus ducimus et debitis quisquam et illum sunt sed odit dignissimos est nulla laborum. In voluptas vero nam voluptas amet voluptatem provident ea nulla voluptatum ut temporibus molestiae aut temporibus explicabo.
+                    Hic dolorum illum ab beatae tempore ad dolorem laborum est quod consequatur id illo quibusdam est neque cupiditate. Eum omnis itaque 33 expedita laborum cum quisquam tempore et culpa eveniet nam voluptatem velit nam doloremque voluptas a voluptas nemo. Et magni obcaecati qui reprehenderit quia consequatur voluptas aut aperiam voluptas.
+                    Qui doloremque beatae eum Quis sequi et atque nihil sed fugit galisum nobis tenetur At sunt placeat ab enim voluptatem. Ut aliquid molestias qui aliquid ab cupiditate omnis At aliquam dolores ut eligendi officia a beatae veniam.
                 </div>
             </div>
             </div>
