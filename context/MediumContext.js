8c89d9d1e3244ee0e45c245ca38e6abc451d9193
Nardin Lachowski <nardin@live.com>
2022-07-26 21:40:16 -0400

    passing the data dynamically from db

--- context/MediumContext.js
+++ context/MediumContext.js
@@ -25,6 +25,30 @@ const MediumProvider = ({ children }) => {
         getUsers()
     }, [])
 
+    useEffect(() => {
+        const getPosts = async () => {
+            const querySnapshot = await getDocs(collection(db, 'articles'))
+            
+            setPosts(querySnapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    data: {
+                        body: doc.data().body,
+                        brief: doc.data().brief,
+                        category: doc.data().category,
+                        postLength: doc.data().postLength,
+                        bannerImage: doc.data().bannerImage,
+                        title: doc.data().title,
+                        // comments: doc.data().comments,
+                        postedOn: doc.data().postedOn.toDate(),
+                        author: doc.data().author,
+                    }
+                }
+            }))
+        }
+        getPosts()
+    }, [])
+
     return (
         <MediumContext.Provider value={{posts, users}}>
             {children}
