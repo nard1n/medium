4770153ca2d7281363b348db0c6bdc186fcf8d33
Nardin Lachowski <nardin@live.com>
2022-07-28 14:12:37 -0400

    fixed up usereffect adding dependency and checking for presence of users

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,22 +10,22 @@ const styles = {
 }
 
 const Post = () => {
-    const {posts, users} = useContext(MediumContext);
+    const  {posts, users } = useContext(MediumContext);
     const router = useRouter();
     const [post, setPost] = useState([]);
     const [author, setAuthor] = useState([]);
 
     useEffect(() => {
         //guard clause
-        if (posts.length === 0) {
+        if (posts.length === 0 || users.length === 0) {
             return
         }
 
 
         setPost(posts.find(post => post.id === router.query.slug))
 
-        setAuthor(users.find(user => user.id === post?.data?.author))
-    }, [])
+        setAuthor(users.find(user => user.id === post.data?.author))
+    }, [post])
 
     return (
         <div className={styles.content}>
