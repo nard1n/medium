b9b29c88b66989447f07771378aa8fe1fd8f7f1d
Nardin Lachowski <nardin@live.com>
2022-07-29 11:30:12 -0400

    added logic to change front end when user logs in and added functionality to 'Write' Modal

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,30 @@
 import Image from 'next/image';
+import { useContext } from 'react';
+import { MediumContext } from '../context/MediumContext';
 import Logo from '../static/logo.png';
+import Modal from 'react-modal';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import PostModal from './PostModal';
+
+Modal.setAppElement('#__next')
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: '#fff',
+        padding: 0,
+        border: 'none', 
+
+    },
+    overlay: {
+        backgroundColor: 'rgba(10, 11, 13, 0.75)',
+    }
+}
 
 const styles = {
     wrapper: 'flex justify-center gap-10 p-5 bg-[#FCC017]',
@@ -11,6 +36,9 @@ const styles = {
 }
 
 const Header = () => {
+    const { handleUserAuth, currentUser } = useContext(MediumContext);
+    const router = useRouter()
+
     return(
         <div className={styles.wrapper}>
             <div className={styles.content}>
@@ -23,13 +51,33 @@ const Header = () => {
                         alt="logo"
                     />
                 </div>
-                <div className={styles.bannerNav}>
-                    <div>Our Story</div>
-                    <div>Membership</div>
-                    <div>Sign In</div>
-                    <div className={styles.accentedButton}>Get Started</div>
-                </div>
+
+                {currentUser ?
+                    (<div className={styles.bannerNav}>
+                        <div>Our Story</div>
+                        <div>Membership</div>
+                        <Link href={('/?addNew=1')}>
+                            <div className={styles.accentedButton}>Write</div>
+                        </Link>
+                        <div className={styles.accentedButton}>Get Unlimited Access</div>
+                    </div>)
+                    : (
+                        <div className={styles.bannerNav}>
+                            <div>Our Story</div>
+                            <div>Membership</div>
+                            <div onClick={handleUserAuth}>Sign In</div>
+                            <div className={styles.accentedButton}>Get Started</div>
+                        </div>
+                    )
+                }
             </div>
+            <Modal
+                isOpen={Boolean(router.query.addNew)}
+                onRequestClose={() => router.push('/')}
+                style={customStyles}
+            >
+                <PostModal />
+            </Modal>
         </div>
     )
 }
