35c3ed89aee75f837804d33c31bd00fb98f199b1
Nardin Lachowski <nardin@live.com>
2022-07-29 12:19:24 -0400

    Added logic with usestate to store user inputs to db

--- components/PostModal.js
+++ components/PostModal.js
@@ -1,14 +1,43 @@
+import { useContext, useState } from "react"
+import { MediumContext } from "../context/MediumContext"
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+
 const styles = {
     wrapper: `w-[70rem] h-[50rem] flex flex-col items-center gap-[1rem] p-[1rem] font-mediumSerif overflow-scroll`,
     title: `my-[2rem] font-bold text-3xl`,
     smallField: `w-full flex justify-between gap-[1rem]`,
     fieldTitle: `flex-1 text-end`,
     inputContainer: `flex-[5] h-min border-2 border-[#787878]`,
+    accentedButton: 'bg-black text-white py-2 px-4 rounded-full',
     inputField: `w-full border-0 outline-none bg-transparent`,
 }   
 
 const PostModal = () => {
-   
+    const { currentUser } = useContext(MediumContext)
+
+    const [title, setTitle] = useState('')
+    const [brief, setBrief] = useState('')
+    const [category, setCategory] = useState('')
+    const [postLength, setPostLength] = useState('')
+    const [bannerImage, setBannerImage] = useState('')
+    const [body, setBody] = useState('')
+
+    const addPostToFirebase = async event => {
+        event.preventDefault()
+
+        await addDoc(collection(db, 'articles'), {
+            bannerImage: bannerImage,
+            body: body,
+            brief: brief,
+            category: category,
+            postedOn: serverTimestamp(),
+            postLength: Number(postLength),
+            title: title,
+            author: currentUser.email
+        })
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>Create a New Post</div>
@@ -18,9 +47,8 @@ const PostModal = () => {
                     <input 
                         className={styles.inputField}
                         type='text'
-                        //value={title}
-                        //onChange
-                    
+                        value={title}
+                        onChange={event => setTitle(event.target.value)}      
                     />
 
                 </span>
@@ -31,9 +59,8 @@ const PostModal = () => {
                     <input 
                         className={styles.inputField}
                         type='text'
-                        //value={title}
-                        //onChange
-                    
+                        value={brief}
+                        onChange={event => setBrief(event.target.value)}      
                     />
 
                 </span>
@@ -44,9 +71,8 @@ const PostModal = () => {
                     <input 
                         className={styles.inputField}
                         type='text'
-                        //value={title}
-                        //onChange
-                    
+                        value={bannerImage}
+                        onChange={event => setBannerImage(event.target.value)}      
                     />
 
                 </span>
@@ -57,9 +83,8 @@ const PostModal = () => {
                     <input 
                         className={styles.inputField}
                         type='text'
-                        //value={title}
-                        //onChange
-                    
+                        value={category}
+                        onChange={event => setCategory(event.target.value)}      
                     />
 
                 </span>
@@ -70,9 +95,8 @@ const PostModal = () => {
                     <input 
                         className={styles.inputField}
                         type='text'
-                        //value={title}
-                        //onChange
-                    
+                        value={postLength}
+                        onChange={event => setPostLength(event.target.value)}      
                     />
 
                 </span>
@@ -84,13 +108,13 @@ const PostModal = () => {
                         className={styles.inputField}
                         type='text'
                         rows={12}
-                        //value={title}
-                        //onChange
-                    
+                        value={body}
+                        onChange={event => setBody(event.target.value)}      
                     />
 
                 </span>
             </div>
+            <button onClick={addPostToFirebase} className={styles.accentedButton}>Submit</button>
 
         </div>
     )
